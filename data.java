548fc6b8df5840db61858341407c795e0993d239
ktamilara <42666475+ktamilara@users.noreply.github.com>
2019-03-19 10:01:34 +0530

    Create data.java

---
+++ data.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class data {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        int count=0;
+    for(int i=0;i<s.length();i++){
+    char ch=s.charAt(i);
+    if(ch!=' '){
+        count++;
+    }
+    }
+        
+  System.out.println(count);    
+    }
+}
