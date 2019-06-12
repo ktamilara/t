ed2cbb77647329fc5408168d852dc8076f93ba06
ktamilara <42666475+ktamilara@users.noreply.github.com>
2019-06-12 15:46:40 +0530

    Update 33p.py

--- 33p.py
+++ 33p.py
@@ -1,26 +1,21 @@
-l1=input()
-t1=[0]
-x1='b1'
-ctr1=0
-if 'ab1' not in l1:
-    print(ct1r)
-else:    
- for i1 in range(len(1l)):
-    ctr1=1
-    for j1 in range(i1,len(l1)-1):
-        if 'a1'==l1[j1] and x1==l1[j1+1]:
-                ctr1=ctr1+1
-           
-        elif x1==l1[j1] and  'a1'==l1[j1+1]:
-                ctr1=ct1r+1
-            
-        else:
-            t1.append(ctr1)
-            ctr1=1
-            break
-    if l1[i1]=='a1':
-        t1.append(ctr1)
-    else:
-        t1.append(ctr1-1)
-      
- print(int(max(t1)))
+s1=input()
+l1=[0]
+if "ab1" not in s1:
+	print("0")
+else:
+	for i1 in range(len(s1)):
+		c1=1
+		for j1 in range(i1,len(s1)-1):
+			if s1[j1]=="a1" and s1[j1+1]=="b":
+				c1=c1+1
+			elif s1[j1]=="b1" and s1[j1+1]=="a1":
+				c1=c1+1
+			else:
+				l1.append(c1)
+				c1=1
+				break
+		if s1[i1]=="a1":
+			l1.append(c1)
+		else:
+			l1.append(c1-1)
+	print(max(l1))
