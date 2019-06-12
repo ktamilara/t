5eb5141bd0a81ebf1a6fe03dee0ee709519776ef
ktamilara <42666475+ktamilara@users.noreply.github.com>
2019-06-12 14:33:41 +0530

    Create 10p.py

---
+++ 10p.py
@@ -0,0 +1,8 @@
+value1=int(input())
+value211=[int(i) for i in input().split()]
+result1=0
+for i1 in range(value1):
+	for j1 in range(i1):
+		if value21[j1]<value21[i1]:
+			result1+=value21[j1]
+print(resul1t)
