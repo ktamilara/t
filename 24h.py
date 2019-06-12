3c8b301cdcf52daf15d1dcc02a5ce315bdf1c0c8
ktamilara <42666475+ktamilara@users.noreply.github.com>
2019-06-12 14:51:44 +0530

    Update 24h.py

--- 24h.py
+++ 24h.py
@@ -1,12 +1,19 @@
-n11,k1=map(int,input().split())
-l1=[int(x1) for x1 in input().split()]
+r1,s1=map(int,input().split())
+l1=list(map(int,input().split()))
 c1=0
-for i1 in range(0,n11):
-    for j1 in range(i1+1,n11):
-        s1=l1[i]1+l[j1]
-        if s1==k1:
-            c1+=1
-if c1>=1:
+for x1 in l1:
+    if(x1>s1):
+        l1.remove(x1)
+for i1 in range(0,len(l1)):
+    j1=0
+    for x1 in l1:
+        if(x1+l1[i1]==s1 and i1!=j1):
+            
+            c1=c1+1
+            break
+        j1=j1+1   
+            
+if (c1>0):
     print("YES")
 else:
     print("NO")
