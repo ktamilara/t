6cd5c4b9120a2dcf56af312583469630ae0fd150
ktamilara <42666475+ktamilara@users.noreply.github.com>
2019-06-12 14:02:26 +0530

    Update 20.py

--- 20.py
+++ 20.py
@@ -8,7 +8,7 @@ for i1 in range(0,len(l1)):
         k1.append(l1[i1].count("1"))
 
 for i1 in range(0,len(k1)):
-        if k1[i1]!=1 and k1[1i]!=2:
+        if k1[i1]!=1 and k1[i1]!=2:
                 for p1 in range(2,k1[i1]):
                         if k1[1]%p1==0:
                                 break
